067541f34586ba3c6385f54651eeef3edcdc2084
lucasdepaula19 <lucasdepaula2012@gmail.com>
2020-05-05 22:57:11 -0300

    criação arquivo readme

---
+++ README.md
@@ -0,0 +1,15 @@
+Comandos
+
+python -m venv virtualenv
+virtualenv\Scripts\activate.bat
+python -m Django --version
+django-admin startproject myProjt1
+dependencias.txt
+python -m pip install -r dependencias.txt
+python manage.py runserver
+python manage.py startapp people
+python manage.py makemigrations people
+python manage.py migrate people
+
+
+https://github.com/ticksmas/django_sample
--- people/models.py
+++ people/models.py
@@ -1,4 +1,17 @@
 from django.db import models
 
 class Pessoa(models.Model):
-    attr1 = "coisa" 
+    nome = models.CharField(max_Length=200)
+    idade = models.IntegerField(default=0)
+    data_nascimento = models.DataField(null=True)
+    cpf = models.CharField(max_Length=14, null=True)
+
+class Endereco(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+    logradouro = models.CharField(max_Length=200)
+    numero = models.IntegerField()
+    cep = models.CharField(max_Length=9)
+
+class Departamento(models.Model):
+
+class Cargo(models.Model):
